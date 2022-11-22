96f76e0011080adb4c592d92b329426964f4cad3
Eltach00 <eltacquliyev@gmail.com>
2022-11-22 16:05:08 +0400

    navigate and location hooks

--- src/Router/Courses.jsx
+++ src/Router/Courses.jsx
@@ -1,11 +1,36 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import courses from '../data/courses'
+import queryString from 'query-string'
+import { useEffect, useState } from 'react'
+
+const SORT_KEYS = ['id', 'slug', 'title']
+
+function sortCourse(courses, key) {
+  if (!key || !SORT_KEYS.includes(key)) {
+    return courses
+  }
+  return [...courses].sort((a, b) => (a[key] > b[key] ? 1 : -1))
+}
 
 const Courses = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const parse = queryString.parse(location.search)
+  const [sortKey, setSortKey] = useState(parse.sort)
+  const [sortedCourses, setSortedCourses] = useState(
+    sortCourse(courses, sortKey)
+  )
+  useEffect(() => {
+    if (!SORT_KEYS.includes(sortKey)) {
+      navigate('.')
+      setSortKey()
+      setSortedCourses([...courses])
+    }
+  }, [navigate, sortKey])
   return (
     <>
-      <h1>Courses</h1>
-      {courses.map((item) => {
+      <h1>{sortKey ? `Courses sorted by ${sortKey}` : 'Courses'}</h1>
+      {sortedCourses.map((item) => {
         return (
           <Link key={item.id} to={item.slug}>
             {item.title}
